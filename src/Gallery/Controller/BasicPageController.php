5c6aad9ae1c4060e008282dbd4e0fed2159c5777
Milan Galesev <milan.galesev@etondigital.com>
2015-05-04 16:01:44 +0200

    Code improvements.

--- src/Gallery/Controller/BasicPageController.php
+++ src/Gallery/Controller/BasicPageController.php
@@ -4,17 +4,11 @@ namespace Gallery\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class BasicPageController
 {
     public function homeAction(Request $request, Application $app)
     {
-//        $artist = $request->attributes->get('artist');
-//        if (!$artist) {
-//            $app->abort(404, 'The requested artist was not found.');
-//        }
-
         $data = array(
           'data' => 'Welcome',
         );
--- src/Gallery/Form/ImageType.php
+++ src/Gallery/Form/ImageType.php
@@ -4,7 +4,6 @@ namespace Gallery\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImageType extends AbstractType
 {
